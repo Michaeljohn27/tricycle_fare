7d338dc1ab4bb53343008f66607c106d661d520c
Michaeljohn27 <mjernacio@sorsu.edu.ph>
2025-10-09 17:33:39 +0800

    Trip Controller and Model created but without function

---
+++ app/Models/Trip.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Trip extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'passenger_id',
+        'driver_id',
+        'trip_status',
+        'start_location',
+        'end_location',
+        'distance',
+    ];
+
+    public function passenger(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'passenger_id');
+    }
+
+    public function driver(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'driver_id');
+    }
+}
